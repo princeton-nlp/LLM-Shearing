82d1d7788fdc7339728ea5f83af06115a4d61315
xiamengzhou <296337231@qq.com>
2023-12-04 18:55:00 -0500

    update

--- llmshearing/models/model_registry.py
+++ llmshearing/models/model_registry.py
@@ -6,6 +6,7 @@ from llmshearing.models.composer_pythia import ComposerMosaicPythia
 
 COMPOSER_MODEL_REGISTRY = {
     'mosaic_llama_125m': ComposerMosaicLlama,
+    'mosaic_llama_370m': ComposerMosaicLlama,
     "mosaic_llama_1.3b": ComposerMosaicLlama,
     "mosaic_llama_3b": ComposerMosaicLlama,
     'mosaic_llama_7b': ComposerMosaicLlama,
@@ -16,6 +17,7 @@ COMPOSER_MODEL_REGISTRY = {
     'mosaic_pythia_160m': ComposerMosaicPythia,
     'mosaic_pythia_410m': ComposerMosaicPythia,
     'mosaic_pythia_1.4b': ComposerMosaicPythia,
+    'mosaic_llama2_370m': ComposerMosaicLlama,
     "mosaic_llama2_1.3b": ComposerMosaicLlama,
     "mosaic_llama2_3b": ComposerMosaicLlama,
     'mosaic_llama2_7b': ComposerMosaicLlama,
---
+++ llmshearing/scripts/test_continue_pretrain.sh
@@ -0,0 +1,99 @@
+# continue pre-train llama2 7b -> 3b or 1.3b
+
+PROJ_DIR=/scratch/gpfs/mengzhou/space2/LLM-Shearing
+DATA_DIR=/scratch/gpfs/mengzhou/llm_data/version5-uint16/500b_dedup_4k/for_ft
+OUTPUT_DIR=/scratch/gpfs/mengzhou/space2/out/test_release
+LAUNCH_SCRIPT=${PROJ_DIR}/llmshearing/scripts/launch.sh
+TRAIN_SCRIPT=${PROJ_DIR}/llmshearing/train.py
+
+test=False
+
+model=370m # target model size
+config_file=${PROJ_DIR}/llmshearing/configs/llama2/${model}.yaml
+prune_run_name=llama2_7b_pruning_scaling_doremi_to${model}_sl4096
+path=${OUTPUT_DIR}/${prune_run_name}/pruned-latest-rank0.pt # path to the 
+# pruned model
+path=/scratch/gpfs/mengzhou/space2/out/test_release_pruning/llama2_7b_pruning_scaling_doremi_to370m_sl4096/pruned-latest-rank0.pt
+
+# data setup
+data_local=${DATA_DIR}
+
+# basic setup
+max_seq_len=4096
+device_train_microbatch_size=16
+global_train_batch_size=256
+device_eval_batch_size=8
+
+# learning setup
+lr=1e-4 # learning rate for the main parameters
+max_duration=48000ba # 50B tokens
+save_interval=3200ba # save every 3200ba
+t_warmup=1440ba # 3% learning rate warmup 
+
+# dynamic loading setup
+dynamic=True
+set_names=[cc,github,book,stackexchange,wiki,arxiv,c4-rp] # domain names
+proportion=[0.2192,0.0002,0.0791,0.0064,0.0096,0.001,0.6845] # final proportion of pruning
+# doremi: update weights with exponential descent
+# constant: keep the weights constant
+update_type=doremi 
+if [[ $to_model == 1.3b ]]; then
+    target_loss=[1.9643,0.7459,2.1393,1.6117,1.7590,1.4449,2.1251] # 1.3b predicted loss from scaling law
+else
+    target_loss=[1.8712,0.6883,2.0325,1.5353,1.6297,1.3560,2.0328] # 2.7b predicted loss from scaling law
+elif [[ $to_model == 370m ]]; then
+    target_loss=[2.1401,0.8694,2.3625,1.7791,2.047,1.6637,2.3139] # 370m predicted loss from scaling law
+fi
+eval_split_name=eval_merge # eval on all domains
+eval_interval=400ba # eval every 50 batches and update the loading proportion
+
+
+# save directroy
+run_name=${prune_run_name}_ft${max_duration}
+save_dir=${OUTPUT_DIR}/${run_name}
+wandb_dir=${save_dir} # save locally
+
+if [[ $test == True ]]; then t=00-1:00:00; else t=01-00:00:00; fi
+
+# Run in bash, it will automatically use resources available in the current environment
+# composer $TRAIN_SCRIPT \
+
+
+# Run with slurm
+sbatch --job-name ${run_name} \
+    --nodes=8 \
+    --gpus-per-node=2 \
+    --mem=512gb \
+    --cpus-per-task=8 \
+    --time $t \
+    --dependency=singleton \
+    $LAUNCH_SCRIPT \
+    $config_file \
+    run_name=${run_name} \
+    data_local=${data_local} \
+    eval_loader.dataset.split=${eval_split_name} \
+    global_train_batch_size=${global_train_batch_size} \
+    device_train_microbatch_size=${device_train_microbatch_size} \
+    device_eval_batch_size=${device_eval_batch_size} \
+    max_seq_len=${max_seq_len} \
+    max_duration=${max_duration} \
+    eval_first=true \
+    scheduler.t_warmup=${t_warmup} \
+    save_folder=${save_dir} \
+    loggers.wandb.init_kwargs.dir=${wandb_dir} \
+    eval_interval=${eval_interval} \
+    save_interval=${save_interval} \
+    optimizer.lr=${lr} \
+    model.l0_module=null \
+    model.path=${path} \
+    callbacks.data_loading.dynamic=${dynamic} \
+    callbacks.data_loading.set_names=${set_names} \
+    callbacks.data_loading.proportion=${proportion} \
+    callbacks.data_loading.update_type=${update_type} \
+    callbacks.data_loading.target_loss=${target_loss} \
+    train_loader.num_workers=0 \
+    train_loader.prefetch_factor=null \
+    train_loader.persistent_workers=false \
+    autoresume=true
+
+# checking eval_first
